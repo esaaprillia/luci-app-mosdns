c6626fad0be5f9d614fe95e6acf9808632b95f92
esaaprillia <esaapriliasalsabila@gmail.com>
2024-09-15 15:03:09 +0800

    Update meson.mk

--- meson/meson.mk
+++ meson/meson.mk
@@ -98,7 +98,6 @@ endef
 define Host/Configure/Meson
 	$(call Meson/CreateNativeFile,$(HOST_BUILD_DIR)/openwrt-native.txt)
 	$(call Meson, \
-		setup \
 		--native-file $(HOST_BUILD_DIR)/openwrt-native.txt \
 		-Ddefault_library=static \
 		$(MESON_HOST_ARGS) \
@@ -123,7 +122,6 @@ define Build/Configure/Meson
 	$(call Meson/CreateNativeFile,$(PKG_BUILD_DIR)/openwrt-native.txt)
 	$(call Meson/CreateCrossFile,$(PKG_BUILD_DIR)/openwrt-cross.txt)
 	$(call Meson, \
-		setup \
 		--buildtype plain \
 		--native-file $(PKG_BUILD_DIR)/openwrt-native.txt \
 		--cross-file $(PKG_BUILD_DIR)/openwrt-cross.txt \
