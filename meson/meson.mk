6276e1cf06bd58887e83b9d2e16e67a760353eeb
esaaprillia <esaapriliasalsabila@gmail.com>
2024-11-28 08:35:39 +0800

    numpy

--- meson/meson.mk
+++ meson/meson.mk
@@ -97,6 +97,7 @@ endef
 
 define Host/Configure/Meson
 	$(call Meson/CreateNativeFile,$(HOST_BUILD_DIR)/openwrt-native.txt)
+	$(call Meson/CreateCrossFile,$(PKG_BUILD_DIR)/openwrt-cross.txt)
 	$(call Meson, \
 		setup \
 		--native-file $(HOST_BUILD_DIR)/openwrt-native.txt \
